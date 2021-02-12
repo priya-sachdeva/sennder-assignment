f335631c4998518de9d9e2f7c901135fb2a2b181
priya-sachdeva <78845251+priya-sachdeva@users.noreply.github.com>
2021-02-12 17:52:11 +0530

    updated files

--- functions/sennder.py
+++ functions/sennder.py
@@ -30,50 +30,50 @@ class Sennder:
         return browser.find_elements_by_css_selector(cssSelector)
 
     def login(browser, username, pwd):
-        waitForElement(browser, email_textbox)
-        emailadd = getElementByCssSelector(browser, email_textbox)
+        waitForElement(browser, selector.email_textbox)
+        emailadd = getElementByCssSelector(browser, selector.email_textbox)
         emailadd.send_keys(username)
-        password = getElementByCssSelector(browser, password_textbox)
+        password = getElementByCssSelector(browser, selector.password_textbox)
         password.send_keys(pwd)
-        loginbutton = getElementByCssSelector(browser, login_button)
+        loginbutton = getElementByCssSelector(browser, selector.login_button)
         loginbutton.click()
 
     def createboard(browser):
-        session_name = getElementByCssSelector(browser, session_textbox)
+        session_name = getElementByCssSelector(browser, selector.session_textbox)
         session_name.send_keys('My first board')
-        select = Select(getElementByCssSelector(browser, owner))
+        select = Select(getElementByCssSelector(browser, selector.owner))
         select.select_by_visible_text('Sennder')
-        waitForElement(browser, create_button).click()
-        waitForElement(browser, created_popup)
-        assert getElementByCssSelector(browser, created_text).text == "Created"
+        waitForElement(browser, selector.create_button).click()
+        waitForElement(browser, selector.created_popup)
+        assert getElementByCssSelector(browser, selector.created_text).text == "Created"
         assert browser.current_url == "https://sprintboards.io/boards/create"
 
     def createcard(browser, title, description=None):
-        card_title = getElementByCssSelector(browser, title_textbox)
+        card_title = getElementByCssSelector(browser, selector.title_textbox)
         card_title.send_keys(title)
         if description is not None:
-            goaldesc = getElementByCssSelector(browser, desc_textbox)
+            goaldesc = getElementByCssSelector(browser, selector.desc_textbox)
             goaldesc.send_keys(description)
-        addcardbutton = getElementByCssSelector(browser, card_button)
+        addcardbutton = getElementByCssSelector(browser, selector.card_button)
         addcardbutton.click()
 
     def like(browser):
-        likeButton = getElementByCssSelector(browser, like_button)
+        likeButton = getElementByCssSelector(browser, selector.like_button)
         assert likeButton.text == "0"
         likeButton.click()
         time.sleep(3)
         assert likeButton.text == "1"
 
     def delete(browser):
-        getElementByCssSelector(browser, delete_button).click()
-        waitForElement(browser, delete_card)
-        deletetitle = getElementByCssSelector(browser, delete_title)
+        getElementByCssSelector(browser, selector.delete_button).click()
+        waitForElement(browser, selector.delete_card)
+        deletetitle = getElementByCssSelector(browser, selector.delete_title)
         assert deletetitle.text == "Delete Card"
-        deletedesc = getElementByCssSelector(browser, delete_desc)
+        deletedesc = getElementByCssSelector(browser, selector.delete_desc)
         assert deletedesc.text == "Are you sure you want to continue?"
-        deletebutton = getElementByCssSelector(browser, deletecard_button)
+        deletebutton = getElementByCssSelector(browser, selector.deletecard_button)
         deletebutton.click()
         time.sleep(5)
-        assert len(getElementsByCssSelector(browser, card_number)) == 1
+        assert len(getElementsByCssSelector(browser, selector.card_number)) == 1
 
 
--- tests/test_sennder.py
+++ tests/test_sennder.py
@@ -1,10 +1,15 @@
-from functions.sennder import Sennder
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import chromedriver_binary
+
+from functions.sennder import Sennder
+sennder1 = Sennder()
+
+from data.selectors import Selectors
+selector1 = Selectors()
 
 # Getting credentials from environment variables
 import os
@@ -18,42 +23,42 @@ browser = webdriver.Chrome()
 browser.get("https://sprintboards.io/auth/login")
 
 # Login to the website
-login(browser,username,password)
+sennder1.login(browser,username,password)
 
 #  Create board button and waiting for session textbox
-waitForElement(browser,createboard_button).click()
-waitForElement(browser,session_textbox)
+sennder1.waitForElement(browser,selector1.createboard_button).click()
+sennder1.waitForElement(browser,selector1. session_textbox)
 
 # Check Browser Title and URL
 assert browser.current_url == "https://sprintboards.io/boards/create"
 assert "Create a Board" in browser.title
 
 # Create Board
-createboard(browser)
+sennder1.createboard(browser)
 
 # Add green card
-waitForElement(browser,green_button).click()
-waitForElement(browser,"body > div.fade.modal.show > div > div")
-assert getElementByCssSelector(browser, "#add-card-modal").text == "Add a Card"
-createcard(browser, 'Goal was achieved', 'Sprint was well planned')
-waitForElement(browser, green_card)
-assert getElementByCssSelector(browser,greencard_title).text == "Goal was achieved"
-assert getElementByCssSelector(browser,greencard_desc).text == "Sprint was well planned"
+sennder1.waitForElement(browser,selector1.green_button).click()
+sennder1.waitForElement(browser,"body > div.fade.modal.show > div > div")
+assert sendder1.getElementByCssSelector(browser, "#add-card-modal").text == "Add a Card"
+sennder1.createcard(browser, 'Goal was achieved', 'Sprint was well planned')
+sennder1.waitForElement(browser, selector1.green_card)
+assert sennder1.getElementByCssSelector(browser,selector1.greencard_title).text == "Goal was achieved"
+assert sennder1.getElementByCssSelector(browser,selector1.greencard_desc).text == "Sprint was well planned"
 
 # Add red card
-getElementByCssSelector(browser,red_button).click()
-waitForElement(browser,"body > div.fade.modal.show > div > div")
-assert getElementByCssSelector(browser,"#add-card-modal").text == "Add a Card"
-createcard(browser, 'Goal was not achieved')
-waitForElement(browser,red_card)
-assert  getElementByCssSelector(browser,redcard_title).text == "Goal was not achieved"
-assert  getElementByCssSelector(browser,redcard_desc).text == "No description provided."
+sennder1.getElementByCssSelector(browser,red_button).click()
+sennder1.waitForElement(browser,"body > div.fade.modal.show > div > div")
+assert sennder1.getElementByCssSelector(browser,"#add-card-modal").text == "Add a Card"
+sennder1.createcard(browser, 'Goal was not achieved')
+sennder1.waitForElement(browser,selector1.red_card)
+assert sennder1.getElementByCssSelector(browser,selector1.redcard_title).text == "Goal was not achieved"
+assert sennder1.getElementByCssSelector(browser,selector1.redcard_desc).text == "No description provided."
 
 # Like green card
-like(browser)
+sennder1.like(browser)
 
 # Delete red card
-delete(browser)
+sennder1.delete(browser)
 
 # Exit browser
 browser.quit()
